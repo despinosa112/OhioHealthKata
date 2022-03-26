14dd294330b4e96e4180900efd2470e5ffc5c0a3
Daniel Espinosa <despinosa112@gmail.com>
2022-03-26 14:20:53 -0400

    Initial Commit

---
+++ OhioHealthKata/OhioHealthKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhioHealthKataApp.swift
+//  OhioHealthKata
+//
+//  Created by Daniel Espinosa on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct OhioHealthKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
